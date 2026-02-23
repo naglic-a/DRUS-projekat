e77749172be2efc13acce726c425da39641ade7e
naglic-a <aleksanaglic@gmail.com>
2026-02-23 22:02:10 +0100

    Proof of concept sensor and server Apps

--- SensorApp/Program.cs
+++ SensorApp/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Text.Json;
+using SharedUtils.DTOs;
+
+namespace SensorApp;
+
+class Program {
+    static async Task Main(string[] args) {
+        string serverIp = "127.0.0.1"; // nekako Preko env varijabli?
+        int port = 8080;
+        string mySensorId = "1423"; // nekako Preko env varijabli?
+
+        Console.WriteLine($"{mySensorId} starting up....");
+
+        try {
+            using TcpClient client = new TcpClient();
+            await client.ConnectAsync(serverIp, port);
+            Console.WriteLine($"Connected to server at {serverIp}:{port}");
+
+            using NetworkStream stream = client.GetStream();
+            using StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+
+            Random rand = new Random();
+
+            while(true) {
+                var reading = new SensorReadingDto {
+                    SensorId = mySensorId,
+                    Value = decimal(rnd.NextDouble() * (85.9 - 20.0) + 20.0),
+                    Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+                };
+
+                string jsonMessage = JsonSerializer.Serialize(reading);
+
+                await writer.WriteLineAsync(jsonMessage);
+                Console.WriteLine($"sent {jsonMessage}");
+
+                await Task.Delay(2000);
+            }
+        } catch(Exception ex) {
+            Console.WriteLine($"Error, could not connect to server: {ex.Message}");
+        }
+    }
+}
--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using SharedUtils.DTOs;
+using Serilog;
+
+namespace ServerApp;
+
+class Program {
+    static async Task Main(string[] args) {
+        int port = 8080;
+        TcpListener listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+        Console.WriteLine($"Listening on port {port}...");
+    
+        while(true) {
+            TcpClient client = await listener.AcceptTcpClientAsync();
+
+            _ = HandleClientAsync(client);
+        }
+    }
+
+    static async Task HandleClientAsync(TcpClient client) {
+        using (client)
+        using (NetworkStream stream = client.GetStream())
+        using (StreamReader reader = new StreamReader(stream)) {
+            try {
+                string? line;
+                while((line = await reader.ReadLineAsync()) != null) {
+                    var reading = JsonSerializer.Deserialize<SensorReadingDto>(line);
+
+                    if(reading != null) {
+                        Console.WriteLine($"Received reading: {reading.SensorId}, Temp: {reading.Value} C");
+                    }
+                }
+            } catch(Exception ex) {
+                Console.WriteLine($"Connection lost: {ex.Message}");
+            }
+        }
+
+        Console.WriteLine("Sensore disconnected...");
+    }
+}
